d0fa6ea92b105ffa882e91479233eafbdd51a717
Aleksandr Pavlov <sanek_pavlov03@mnail.ru>
2024-03-18 15:20:37 +0400

    add search by videos

---
+++ src/collections/VideosCollection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Search\Sdk\collections;
+
+class VideosCollection extends Collection
+{
+    protected string $prefix = 'videos';
+
+    protected string $titleField = 'title';
+}
---
+++ src/models/Video.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Search\Sdk\Models;
+
+class Video extends Model
+{
+    protected array $params = [
+        'title'=>'Название',
+        'description' => 'Описание',
+    ];
+
+    protected array $intParams = [];
+}
